8e6837ff619dc3dddde0d4e33069bfaa481bf8e3
Roy Penrod <roypenrod123@yahoo.com>
2015-12-30 16:50:17 -0600

    used Step to serialize FB API call with function to render it -- code now works

--- routes/index.js
+++ routes/index.js
@@ -8,36 +8,40 @@ router.get('/', function(req, res, next) {
     
     // accesses the vars exported in app.js
     var FB = req.app.get('FB');
-                
-    // runs the FB Graph API call and handles the response
-    FB.api('corebrewery', { fields: ['id', 'name', 'mission'] }, function (jsonRes) {
-        if(!jsonRes || jsonRes.error) {
-            console.log(!jsonRes ? 'error occurred' : jsonRes.error);
-            return;
-        }
-        console.log('==========================');
-        console.log("inside API Call(jsonRes.id): " + jsonRes.id);
-        console.log("inside API Call(jsonRes.name): " + jsonRes.name);
-        console.log("inside API Call(jsonRes.mission:): ");
-        console.log(jsonRes.mission);
-        console.log(' ');
-        console.log("inside API Call(jsonRes): ");
-        console.log(jsonRes);
-        console.log(' ');
-        console.log('==========================');  
-        
-        req.app.set('jsonRes', jsonRes);
+    var Step = req.app.get('Step');
+    
+    Step(
+        function getPageData() {
+            var self = this;
+            
+            // runs the FB Graph API call 
+            // receives the JSON file as a reponse
+            FB.api('corebrewery', { fields: ['id', 'name', 'mission'] }, function (jsonRes) {
+                if(!jsonRes || jsonRes.error) {                    
+                    self(new Error('Error with JSON data returned from FB..'));                    
+                } else {
+                    self(null, jsonRes);
+                }
+                console.log('==========================');
+                console.log("inside API Call(jsonRes.id): " + jsonRes.id);
+                console.log("inside API Call(jsonRes.name): " + jsonRes.name);
+                console.log("inside API Call(jsonRes.mission:): ");
+                console.log(jsonRes.mission);
+                console.log(' ');
+                console.log("inside API Call(jsonRes): ");
+                console.log(jsonRes);
+                console.log(' ');
+                console.log('==========================');  
        
-    });
-    
-    var jsonData = req.app.get('jsonRes');
-    
-    console.log("----------------------------");   
-    console.log("outside API Call(jsonData): ");
-    console.log(jsonData);
-    console.log("----------------------------");
-    console.log("outside API Call (res.name): " + jsonData.name);    
-    res.render('index', { title: 'Connect to FB API', name: jsonData.name, mission: jsonData.mission });
+            });
+            
+        }, 
+        // processes the JSON data from FB
+        // renders it to the template
+        function processData(err, jsonData) {
+            res.render('index', { title: 'Connect to FB API', name: jsonData.name, mission: jsonData.mission });
+        }
+    );
     
 });
 
