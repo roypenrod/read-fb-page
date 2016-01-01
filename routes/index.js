141bb75398919d37be32e24d3920e643352b5f45
Roy Penrod <roypenrod123@yahoo.com>
2016-01-01 07:29:05 -0600

    added 3 most recent photos to profile page layout

--- routes/index.js
+++ routes/index.js
@@ -99,19 +99,96 @@ router.get('/', function(req, res, next) {
        
             });
         },
+        // retrieves the most recent photo from FB in JSON format
+        function getRecentPhotoJSON1 (err, pageDataJSON, photoIdJSON) {
+            
+            var self = this;
+            
+            // runs the FB Graph API call
+            // receives the JSON file as a response
+            FB.api('/' + photoIdJSON.data[0].id, { fields: 'images' },  function (jsonRes) {
+                if(!jsonRes || jsonRes.error) {                    
+                    self(new Error('getRecentPhotoJSON1 Error: Problem with JSON data returned from FB.'));                    
+                } else {
+                    self(null, pageDataJSON, photoIdJSON, jsonRes);
+                }
+//                console.log('==========================');
+//                console.log(' ');
+//                console.log(jsonRes);
+//                console.log(' ');                
+//                console.log('==========================');  
+       
+            });
+            
+        },
+        // retrieves the second most recent photo from FB in JSON format
+        function getRecentPhotoJSON2(err, pageDataJSON, photoIdJSON, recentPhoto1JSON) {
+                        
+            var self = this;
+                        
+            // runs the FB Graph API call
+            // receives the JSON file as a response
+            FB.api('/' + photoIdJSON.data[1].id, { fields: 'images' },  function (jsonRes) {
+                if(!jsonRes || jsonRes.error) {                    
+                    self(new Error('getRecentPhotoJSON2 Error: Problem with JSON data returned from FB.'));                    
+                } else {
+                    self(null, pageDataJSON, photoIdJSON, recentPhoto1JSON, jsonRes);
+                }
+//                console.log('==========================');
+//                console.log(' ');
+//                console.log(jsonRes);
+//                console.log(' ');                
+//                console.log('==========================');  
+       
+            });
+                        
+        },
+        // retrieves the third most recent photo from FB in JSON format
+        function getRecentPhotoJSON3(err, pageDataJSON, photoIdJSON, recentPhoto1JSON, recentPhoto2JSON) {
+                        
+            var self = this;
+                        
+            // runs the FB Graph API call
+            // receives the JSON file as a response
+            FB.api('/' + photoIdJSON.data[2].id, { fields: 'images' },  function (jsonRes) {
+                if(!jsonRes || jsonRes.error) {                    
+                    self(new Error('getRecentPhotoJSON3 Error: Problem with JSON data returned from FB.'));                    
+                } else {
+                    self(null, pageDataJSON, photoIdJSON, recentPhoto1JSON, recentPhoto2JSON, jsonRes);
+                }
+//                console.log('==========================');
+//                console.log(' ');
+//                console.log(jsonRes);
+//                console.log(' ');                
+//                console.log('==========================');  
+       
+            });        
+        },
         // processes the JSON data from FB
         // renders it to the template
-        function processData(err, pageDataJSON, photoIdJSON) {
+        function processData(err, pageDataJSON, photoIdJSON, recentPhoto1JSON, recentPhoto2JSON, recentPhoto3JSON) {
             
-            console.log('!!!!!!!!!!!!!!!!!!!!!!!!!!');
-            console.log(' ');
-            console.log(pageDataJSON);
-            console.log(' ');
-            console.log(' 000000000000000000000000 ');
-            console.log(' ');
-            console.log(photoIdJSON);
-            console.log(' ');                
-            console.log('!!!!!!!!!!!!!!!!!!!!!!!!!!');  
+//            console.log('!!!!!!!!!!!!!!!!!!!!!!!!!!');
+//            console.log(' ');
+//            console.log(pageDataJSON);
+//            console.log(' ');
+//            console.log(' 000000000000000000000000 ');
+//            console.log(' ');
+//            console.log(photoIdJSON);
+//            console.log(' ');                
+//            console.log(' 000000000000000000000000 ');
+//            console.log(' ');
+//            console.log(recentPhoto1JSON);
+//            console.log(' ');                            
+//            console.log(' 000000000000000000000000 ');
+//            console.log(' ');
+//            console.log(recentPhoto2JSON);
+//            console.log(' ');   
+//            console.log(' 000000000000000000000000 ');
+//            console.log(' ');
+//            console.log(recentPhoto3JSON);
+//            console.log(' ');   
+//            console.log('!!!!!!!!!!!!!!!!!!!!!!!!!!');  
             
             // break out the category list into new var
             var category = fbExtractCategoryFromList (pageDataJSON.category_list);
@@ -121,7 +198,41 @@ router.get('/', function(req, res, next) {
             
             // splits the awards into an array of strings
             var awards = splitStringIntoArray(pageDataJSON.awards, '\n\n');
-                        
+            
+            // iterate through the recentPhoto1JSON images array and pull out the 
+            // image with a width of 480px
+            var recentPhoto1URL = '';
+            
+            recentPhoto1JSON.images.forEach(function get480px(element, index, array) {
+                
+                if (element.width == 480) {
+                    recentPhoto1URL = element.source;                    
+                }
+            });
+            
+            // iterate through the recentPhoto2JSON images array and pull out the 
+            // image with a width of 480px
+            var recentPhoto2URL = '';
+            
+            recentPhoto2JSON.images.forEach(function get480px(element, index, array) {
+                
+                if (element.width == 480) {
+                    recentPhoto2URL = element.source;                    
+                }
+            });
+            
+            // iterate through the recentPhoto3JSON images array and pull out the 
+            // image with a width of 480px
+            var recentPhoto3URL = '';
+            
+            recentPhoto3JSON.images.forEach(function get480px(element, index, array) {
+                
+                if (element.width == 480) {
+                    recentPhoto3URL = element.source;                    
+                }
+            });
+                    
+            // pass the variables to the template            
             res.render('profile', { title: pageDataJSON.name + " -- Profile", 
                                     name: pageDataJSON.name,
                                     pictureURL: pageDataJSON.picture.data.url,
@@ -141,7 +252,10 @@ router.get('/', function(req, res, next) {
                                     city: pageDataJSON.location.city,
                                     state: pageDataJSON.location.state,
                                     zipCode: pageDataJSON.location.zip,
-                                    reviewsURL: 'http://www.facebook.com/' + fbPageAlias + '/reviews'
+                                    reviewsURL: 'http://www.facebook.com/' + fbPageAlias + '/reviews',
+                                    recentPhoto1URL: recentPhoto1URL,
+                                    recentPhoto2URL: recentPhoto2URL,
+                                    recentPhoto3URL: recentPhoto3URL
                                 });
         }
     );    
